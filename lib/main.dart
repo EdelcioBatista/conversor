4626c5e1d952724a0b1434ea94a2a2f00a2eb073
Edelcio Batista <edelcio.batista@gmail.com>
2019-10-03 21:51:04 -0300

    Excluindo a minha chave

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/finance?format=json&key=b3e19cb5";
+const request = "https://api.hgbrasil.com/finance?format=json&key=<sua_chave>";
 
 
 void main() async {
